9853cb065f7af9097824e6bf97d2a60bbccb60b3
Oswal-Fuentes <oswal_f@unitec.edu>
2018-12-06 20:48:38 -0600

    Conexion y CRUD de MySQL

---
+++ src/MySQL/DB_Connection.java
@@ -0,0 +1,24 @@
+package MySQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB_Connection {
+
+    public static void main(String[] args) {
+        DB_Connection obj_DB_Connection = new DB_Connection();
+        System.out.println(obj_DB_Connection.get_connection());
+    }
+
+    public Connection get_connection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud?autoReconnect=true&useSSL=false", "root", "1234");
+            //jdbc:mysql://localhost:3306/Peoples?autoReconnect=true&useSSL=false
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
---
+++ src/MySQL/Test_Main.java
@@ -0,0 +1,20 @@
+package MySQL;
+
+public class Test_Main {
+
+    public static void main(String[] args) {
+        MySQL objTest = new MySQL();
+        //insert data
+        //objTest.create_data("1", "Marco", "marco@gmail.com");
+        //objTest.create_data("2", "Jorge", "jorge@gmail.com");
+        
+        //read data
+        //objTest.read_data("1");
+        
+        //update data
+        //objTest.update_data("2", "15", "Jose", "jose@gmail");
+        
+        //delete data
+        //objTest.delete_data("16");
+    }
+}
