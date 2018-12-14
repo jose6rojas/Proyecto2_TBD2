c13bdac4158ae8a1cebd641cd7f17f1a89f3cb0f
Oswal-Fuentes <oswal_f@unitec.edu>
2018-12-14 11:38:07 -0600

    Conexion con el GUI

--- src/MySQL/DB_Connection.java
+++ src/MySQL/DB_Connection.java
@@ -5,20 +5,31 @@ import java.sql.DriverManager;
 
 public class DB_Connection {
 
-    public static void main(String[] args) {
-        DB_Connection obj_DB_Connection = new DB_Connection();
-        System.out.println(obj_DB_Connection.get_connection());
-    }
+    private String BD = "", port = "", username = "", password = "";
 
+//    public static void main(String[] args) {
+//        DB_Connection obj_DB_Connection = new DB_Connection();
+//        System.out.println(obj_DB_Connection.get_connection());
+//    }
     public Connection get_connection() {
         Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud?autoReconnect=true&useSSL=false", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/" + BD + "?autoReconnect=true&useSSL=false", username, password);
             //jdbc:mysql://localhost:3306/Peoples?autoReconnect=true&useSSL=false
         } catch (Exception e) {
             System.out.println(e);
         }
         return connection;
+    }
+
+    public DB_Connection() {
+    }
+
+    public DB_Connection(String BD, String port, String username, String password) {
+        this.BD = BD;
+        this.port = port;
+        this.username = username;
+        this.password = password;
     }
 }
